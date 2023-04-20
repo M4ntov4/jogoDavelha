3dbba659b38fe9da56302b4b3d4f9a01a0555ba8
João Vitor Delaia <115042912+M4ntov4@users.noreply.github.com>
2023-04-20 17:27:54 -0300

    Add files via upload

---
+++ jogodaVelha/jogodaVelha/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿
+namespace jogodaVelha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTitulo = new System.Windows.Forms.Label();
+            this.btnum1 = new System.Windows.Forms.Button();
+            this.btnum2 = new System.Windows.Forms.Button();
+            this.btnum3 = new System.Windows.Forms.Button();
+            this.btnum6 = new System.Windows.Forms.Button();
+            this.btnum5 = new System.Windows.Forms.Button();
+            this.btnum4 = new System.Windows.Forms.Button();
+            this.btnum9 = new System.Windows.Forms.Button();
+            this.btnum8 = new System.Windows.Forms.Button();
+            this.btnum7 = new System.Windows.Forms.Button();
+            this.btselcX = new System.Windows.Forms.Button();
+            this.btJogar = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // txtTitulo
+            // 
+            this.txtTitulo.AutoSize = true;
+            this.txtTitulo.Location = new System.Drawing.Point(211, 9);
+            this.txtTitulo.Name = "txtTitulo";
+            this.txtTitulo.Size = new System.Drawing.Size(125, 13);
+            this.txtTitulo.TabIndex = 10;
+            this.txtTitulo.Text = "Jogo da Velha do JayJay";
+            // 
+            // btnum1
+            // 
+            this.btnum1.Enabled = false;
+            this.btnum1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnum1.Location = new System.Drawing.Point(83, 52);
+            this.btnum1.Name = "btnum1";
+            this.btnum1.Size = new System.Drawing.Size(121, 105);
+            this.btnum1.TabIndex = 11;
+            this.btnum1.UseVisualStyleBackColor = true;
+            // 
+            // btnum2
+            // 
+            this.btnum2.Enabled = false;
+            this.btnum2.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnum2.Location = new System.Drawing.Point(210, 52);
+            this.btnum2.Name = "btnum2";
+            this.btnum2.Size = new System.Drawing.Size(121, 105);
+            this.btnum2.TabIndex = 12;
+            this.btnum2.UseVisualStyleBackColor = true;
+            // 
+            // btnum3
+            // 
+            this.btnum3.Enabled = false;
+            this.btnum3.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnum3.Location = new System.Drawing.Point(337, 52);
+            this.btnum3.Name = "btnum3";
+            this.btnum3.Size = new System.Drawing.Size(121, 105);
+            this.btnum3.TabIndex = 13;
+            this.btnum3.UseVisualStyleBackColor = true;
+            // 
+            // btnum6
+            // 
+            this.btnum6.Enabled = false;
+            this.btnum6.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnum6.Location = new System.Drawing.Point(337, 163);
+            this.btnum6.Name = "btnum6";
+            this.btnum6.Size = new System.Drawing.Size(121, 105);
+            this.btnum6.TabIndex = 16;
+            this.btnum6.UseVisualStyleBackColor = true;
+            // 
+            // btnum5
+            // 
+            this.btnum5.Enabled = false;
+            this.btnum5.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnum5.Location = new System.Drawing.Point(210, 163);
+            this.btnum5.Name = "btnum5";
+            this.btnum5.Size = new System.Drawing.Size(121, 105);
+            this.btnum5.TabIndex = 15;
+            this.btnum5.UseVisualStyleBackColor = true;
+            // 
+            // btnum4
+            // 
+            this.btnum4.Enabled = false;
+            this.btnum4.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnum4.Location = new System.Drawing.Point(83, 163);
+            this.btnum4.Name = "btnum4";
+            this.btnum4.Size = new System.Drawing.Size(121, 105);
+            this.btnum4.TabIndex = 14;
+            this.btnum4.UseVisualStyleBackColor = true;
+            // 
+            // btnum9
+            // 
+            this.btnum9.Enabled = false;
+            this.btnum9.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnum9.Location = new System.Drawing.Point(337, 274);
+            this.btnum9.Name = "btnum9";
+            this.btnum9.Size = new System.Drawing.Size(121, 105);
+            this.btnum9.TabIndex = 19;
+            this.btnum9.UseVisualStyleBackColor = true;
+            // 
+            // btnum8
+            // 
+            this.btnum8.Enabled = false;
+            this.btnum8.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnum8.Location = new System.Drawing.Point(210, 274);
+            this.btnum8.Name = "btnum8";
+            this.btnum8.Size = new System.Drawing.Size(121, 105);
+            this.btnum8.TabIndex = 18;
+            this.btnum8.UseVisualStyleBackColor = true;
+            // 
+            // btnum7
+            // 
+            this.btnum7.Enabled = false;
+            this.btnum7.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnum7.Location = new System.Drawing.Point(83, 274);
+            this.btnum7.Name = "btnum7";
+            this.btnum7.Size = new System.Drawing.Size(121, 105);
+            this.btnum7.TabIndex = 17;
+            this.btnum7.UseVisualStyleBackColor = true;
+            // 
+            // btselcX
+            // 
+            this.btselcX.Location = new System.Drawing.Point(127, 455);
+            this.btselcX.Name = "btselcX";
+            this.btselcX.Size = new System.Drawing.Size(124, 33);
+            this.btselcX.TabIndex = 20;
+            this.btselcX.Text = "Escolher Simbolo";
+            this.btselcX.UseVisualStyleBackColor = true;
+            this.btselcX.Click += new System.EventHandler(this.btselcX_Click);
+            // 
+            // btJogar
+            // 
+            this.btJogar.Location = new System.Drawing.Point(268, 455);
+            this.btJogar.Name = "btJogar";
+            this.btJogar.Size = new System.Drawing.Size(124, 33);
+            this.btJogar.TabIndex = 21;
+            this.btJogar.Text = "Jogar";
+            this.btJogar.UseVisualStyleBackColor = true;
+            this.btJogar.Click += new System.EventHandler(this.btJogar_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Location = new System.Drawing.Point(83, 52);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(375, 327);
+            this.panel1.TabIndex = 22;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(547, 530);
+            this.Controls.Add(this.btJogar);
+            this.Controls.Add(this.btselcX);
+            this.Controls.Add(this.btnum9);
+            this.Controls.Add(this.btnum8);
+            this.Controls.Add(this.btnum7);
+            this.Controls.Add(this.btnum6);
+            this.Controls.Add(this.btnum5);
+            this.Controls.Add(this.btnum4);
+            this.Controls.Add(this.btnum3);
+            this.Controls.Add(this.btnum2);
+            this.Controls.Add(this.btnum1);
+            this.Controls.Add(this.txtTitulo);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label txtTitulo;
+        private System.Windows.Forms.Button btnum1;
+        private System.Windows.Forms.Button btnum2;
+        private System.Windows.Forms.Button btnum3;
+        private System.Windows.Forms.Button btnum6;
+        private System.Windows.Forms.Button btnum5;
+        private System.Windows.Forms.Button btnum4;
+        private System.Windows.Forms.Button btnum9;
+        private System.Windows.Forms.Button btnum8;
+        private System.Windows.Forms.Button btnum7;
+        private System.Windows.Forms.Button btselcX;
+        private System.Windows.Forms.Button btJogar;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ jogodaVelha/jogodaVelha/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace jogodaVelha
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btJogar_Click(object sender, EventArgs e)
+        {
+            btnum1.Enabled = true;
+            btnum2.Enabled = true;
+            btnum3.Enabled = true;
+            btnum4.Enabled = true;
+            btnum5.Enabled = true;
+            btnum6.Enabled = true;
+            btnum7.Enabled = true;
+            btnum8.Enabled = true;
+            btnum9.Enabled = true;
+        }
+
+        private void btselcX_Click(object sender, EventArgs e)
+        {
+            EscolherAvatar escolha = new EscolherAvatar();
+            this.Hide();
+            escolha.ShowDialog();
+            this.Show();
+
+        }
+    }
+}
